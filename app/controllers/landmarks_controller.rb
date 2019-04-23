37d0d9a82b021fc44f88c082560892d4b1bb904d
Tyler Hunt <tyhunt04@gmail.com>
2019-04-23 04:09:39 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -19,6 +19,7 @@ class LandmarksController < ApplicationController
     erb :'/landmarks/edit'
   end
 
+
   post '/landmarks/:id' do
     @landmark = Landmark.find(params[:id])
     @landmark.name = params['landmark']['name']
@@ -27,9 +28,18 @@ class LandmarksController < ApplicationController
     redirect to "/landmarks/#{@landmark.id}"
   end
 
+
   post '/landmarks' do
     Landmark.create(:name => params[:landmark][:name], :year_completed => params[:landmark][:year_completed])
     redirect to '/landmarks'
+  end
+
+
+  post '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    @landmark.update(params[:landmark])
+    @landmark.save
+    redirect to "/landmarks/#{@landmark.id}"
   end
 
 end
